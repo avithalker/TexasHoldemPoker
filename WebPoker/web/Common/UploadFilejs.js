f7aff32f016a20189a23ae7135288183e1a8542c
avithalker <30372420+avithalker@users.noreply.github.com>
2018-02-02 19:54:18 +0200

    file upload servlet fix

--- WebPoker/web/Common/UploadFilejs.js
+++ WebPoker/web/Common/UploadFilejs.js
@@ -1,19 +1,21 @@
 $(function() { // onload
-    $("#uploadForm").submit(function() {
+    $("#uploadForm").submit(function () {
 
         var file1 = this[0].files[0];
 
         $.ajax({
-            method:'POST',
+            method: 'POST',
             url: this.action,
             processData: false, // Don't process the files
             contentType: false, // Set content type to false as jQuery will tell the server its a query string request
             timeout: 4000,
-            error: function(e) {
+            error: function (e) {
                 console.error("Failed to submit");
                 $("#result").text("Failed to get result from server " + e);
             },
-            success: function(r) {
+            success: function (r) {
                 $("#result").text(r);
             }
         });
+    });
+});
