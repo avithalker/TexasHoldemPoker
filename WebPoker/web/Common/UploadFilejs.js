4453c15e780129cf6755991007eb2eabdd1b8fff
SaharHaltzi <34007523+SaharHaltzi@users.noreply.github.com>
2018-02-03 08:04:03 +0200

    dfdf

--- WebPoker/web/Common/UploadFilejs.js
+++ WebPoker/web/Common/UploadFilejs.js
@@ -1,10 +1,13 @@
-$(function() { // onload
+$(function() { // onload...do
     $("#uploadForm").submit(function() {
 
         var file1 = this[0].files[0];
+        var formData = new FormData();
+        formData.append("fake-key-1", file1);
 
         $.ajax({
             method:'POST',
+            data: formData,
             url: this.action,
             processData: false, // Don't process the files
             contentType: false, // Set content type to false as jQuery will tell the server its a query string request
@@ -17,3 +20,9 @@ $(function() { // onload
                 $("#result").text(r);
             }
         });
+
+        // return value of the submit operation
+        // by default - we'll always return false so it doesn't redirect the user.
+        return false;
+    })
+})
